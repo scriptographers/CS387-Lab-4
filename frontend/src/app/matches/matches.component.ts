340d2da614d09a1fc69427f474ede1c6ab9b939e
devansh-dvj <devansh.dvj@gmail.com>
2022-01-29 12:14:02 +0530

    Getting data to matches route

--- frontend/src/app/matches/matches.component.ts
+++ frontend/src/app/matches/matches.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-matches',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatchesComponent implements OnInit {
 
-  constructor() { }
+  matches: any;
+  page: number = 1;
+  page_size: number = 10;
+  offset: number = 0;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private server: ServerService
+  ) {
+    this.route.paramMap.subscribe(params => {
+      this.page = params.get('page') ? parseInt(params.get('page') as string) : 1;
+      this.page_size = params.get('size') ? parseInt(params.get('size') as string) : 10;
+      this.offset = (this.page - 1) * this.page_size;
+    });
+    console.log(this.page, this.page_size);
+
+    this.matches = [];
+  }
 
   ngOnInit(): void {
+    this.server.get(`/match_list/?size=${this.page_size}&offset=${this.offset}`).subscribe(
+      res => {
+        this.matches = res;
+        console.log(this.matches);
+      }
+    );
   }
 
 }
