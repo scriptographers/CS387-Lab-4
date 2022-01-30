56262ee3297a60b9dcc8961ac8ddb5cb11a844c6
Harshit Varma <varmaharshit2@gmail.com>
2022-01-30 13:41:12 +0530

    q: updated nrr

--- sql/nrr.sql
+++ sql/nrr.sql
@@ -1,36 +1,22 @@
 select *, round((1.0*runs_by_team)/(overs_by_team) - (1.0*runs_against_team)/(overs_against_team), 2) as nrr from
 (
-
-    (
         select 
-            sum(runs_per_over) as runs_by_team, count(*) as overs_by_team 
+            sum(runs_by_over) as runs_by_team, 
+            count(case when overs_by = 0 then null else 1 end) as overs_by_team,
+            sum(runs_against_over) as runs_against_team, 
+            count(case when overs_against = 0 then null else 1 end) as overs_against_team 
         from
         ( 
-            select sum(runs_scored) as runs_per_over
+            select 
+                sum(case when batting.team_id = 1 then runs_scored else 0 end) as runs_by_over,
+                sum(case when bowling.team_id = 1 then runs_scored else 0 end) as runs_against_over,
+                count(case when batting.team_id = 1 then 1 else null end) as overs_by,
+                count(case when bowling.team_id = 1 then 1 else null end) as overs_against
             from ball_by_ball
             left outer join player_match as batting 
             on ball_by_ball.match_id = batting.match_id and ball_by_ball.striker = batting.player_id
             left outer join player_match as bowling 
             on ball_by_ball.match_id = bowling.match_id and ball_by_ball.bowler = bowling.player_id
-            where batting.team_id = 1
             group by ball_by_ball.match_id, innings_no, over_id
         ) as sq1
-    ) as sq3
-
-    natural join 
-
-    (
-        select sum(runs_per_over) as runs_against_team, count(*) as overs_against_team from
-        ( 
-            select sum(runs_scored) as runs_per_over
-            from ball_by_ball
-            left outer join player_match as batting 
-            on ball_by_ball.match_id = batting.match_id and ball_by_ball.striker = batting.player_id
-            left outer join player_match as bowling 
-            on ball_by_ball.match_id = bowling.match_id and ball_by_ball.bowler = bowling.player_id
-            where bowling.team_id = 1
-            group by ball_by_ball.match_id, innings_no, over_id
-        ) as sq2
-    ) as sq4
-
-) as sq5;
+) as sq2;
