d06ef14d92bbc34caf9d06b710cc800461f37222
devansh-dvj <devansh.dvj@gmail.com>
2022-02-01 20:21:34 +0530

    Added compatibility for base env file

--- backend/pool.js
+++ backend/pool.js
@@ -1,5 +1,5 @@
 const Pool = require('pg').Pool;
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path: require('find-config')('.env') });
 
 const pool = new Pool({
   user: process.env.DB_USER,
