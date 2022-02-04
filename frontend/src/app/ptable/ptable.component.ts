84a0f6766e7a352acffc0e2138769135797bdda9
devansh-dvj <devansh.dvj@gmail.com>
2022-02-04 23:39:24 +0530

    Added sorting of ptable and promises for multiple api calls

--- frontend/src/app/ptable/ptable.component.ts
+++ frontend/src/app/ptable/ptable.component.ts
@@ -11,9 +11,10 @@ export class PtableComponent implements OnInit {
 
   season_year: number = 0;
   teams: any;
+  data: any;
   ptable: any;
   displayedColumns: any;
-  loading = true;
+  loading: boolean;
 
   constructor(
     private router: Router,
@@ -25,50 +26,87 @@ export class PtableComponent implements OnInit {
     });
 
     this.teams = [];
+    this.data = {};
     this.ptable = [[]];
+    this.loading = true;
 
     this.displayedColumns = ['team', 'mat', 'won', 'lost', 'tied', 'pts', 'nrr'];
-
   }
 
   ngOnInit(): void {
     this.server.get('/ptable/team_list').subscribe(
       res => {
         this.teams = res;
-        this.load_table();
+        this.load_data();
       }
     );
   }
 
-  load_table(): void {
+  load_data(): void {
+    const apis: any = [];
     this.teams.forEach(
       (team: any) => {
-        this.ptable[team.team_id-1] = {
+        this.data[team.team_id] = {
           'name': team.team_name,
-          'matches': -1,
-          'wins': -1,
-          'losses': -1,
-          'ties': -1,
-          'points': -1,
-          'nrr': -1
+          'matches': 0,
+          'wins': 0,
+          'losses': 0,
+          'ties': 0,
+          'points': 0,
+          'nrr': 0
         };
-        this.server.get('/ptable/points', { 'season_year': this.season_year, 'team_id': team.team_id }).subscribe(
-          res => {
-            this.ptable[team.team_id-1]['matches'] = res[0].matches;
-            this.ptable[team.team_id-1]['wins'] = res[0].wins;
-            this.ptable[team.team_id-1]['losses'] = res[0].losses;
-            this.ptable[team.team_id-1]['ties'] = res[0].ties;
-            this.ptable[team.team_id-1]['points'] = res[0].points;
-          }
-        );
-        this.server.get('/ptable/nrr', { 'season_year': this.season_year, 'team_id': team.team_id }).subscribe(
-          res => {
-            this.ptable[team.team_id-1]['nrr'] = res[0].nrr;
-          }
-        );
+        apis.push(new Promise((resolve: any) => {
+          this.server.get('/ptable/points', { 'season_year': this.season_year, 'team_id': team.team_id }).subscribe(
+            res => {
+              this.data[team.team_id]['matches'] = res[0].matches;
+              this.data[team.team_id]['wins'] = res[0].wins;
+              this.data[team.team_id]['losses'] = res[0].losses;
+              this.data[team.team_id]['ties'] = res[0].ties;
+              this.data[team.team_id]['points'] = res[0].points;
+              resolve();
+            }
+          );
+        }));
+        apis.push(new Promise((resolve: any) => {
+          this.server.get('/ptable/nrr', { 'season_year': this.season_year, 'team_id': team.team_id }).subscribe(
+            res => {
+              this.data[team.team_id]['nrr'] = res[0].nrr;
+              resolve();
+            }
+          );
+        }));
       });
-    console.log(this.ptable);
-    this.loading = false;
+
+    Promise.all(apis).then(() => {
+      console.log(this.data);
+      this.finalize()
+      this.loading = false;
+    });
+  }
+
+  finalize(): void {
+    this.teams.forEach(
+      (team: any) => {
+        if (this.data[team.team_id]['matches'] != 0) {
+          this.ptable.push(this.data[team.team_id]);
+        }
+      }
+    );
+    this.ptable.sort((a: any, b: any) => {
+      if (a.points > b.points) {
+        return -1;
+      } else if (a.points < b.points) {
+        return 1;
+      } else {
+        if (a.nrr > b.nrr) {
+          return -1;
+        } else if (a.nrr < b.nrr) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+    });
   }
 
 }
