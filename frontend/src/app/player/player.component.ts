163ef841ec5437dd143d3204f8afee556b294c44
devansh-dvj <devansh.dvj@gmail.com>
2022-02-06 13:09:22 +0530

    Added api calls for player component

--- frontend/src/app/player/player.component.ts
+++ frontend/src/app/player/player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ServerService } from '../server.service';
 
 @Component({
   selector: 'app-player',
@@ -7,9 +9,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
-  constructor() { }
+  player_id: number = 0;
+  basic_info: any;
+  bat_stat: any;
+  bowl_stat: any;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private server: ServerService
+  ) {
+    this.route.paramMap.subscribe(params => {
+      this.player_id = params.get('player_id') ? parseInt(params.get('player_id') as string) : 0;
+    });
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+
+    this.basic_info = {
+      player_name: '',
+      country_name: '',
+      batting_hand: '',
+      bowling_skill: ''
+    };
+    this.bat_stat = {
+      total_matches: 0,
+      total_runs: 0,
+      total_balls: 0,
+      fours: 0,
+      sixes: 0,
+      total_out: 0,
+      fifties: 0,
+      hundreds: 0,
+      hs: 0,
+      strike_rate: 0.0,
+      average: 0.0
+    };
+    this.bowl_stat = {
+      total_matches: 0,
+      total_runs: 0,
+      total_wickets: 0,
+      total_overs: 0,
+      total_balls: 0,
+      five_wickets: 0,
+      economy: 0.0
+    };
+  }
 
   ngOnInit(): void {
+    this.server.get('/player/player_info', { 'player_id': this.player_id }).subscribe(
+      res => {
+        this.basic_info = res[0];
+        console.log(this.basic_info);
+      }
+    );
+
+    this.server.get('/player/bat_stat', { 'player_id': this.player_id }).subscribe(
+      res => {
+        this.bat_stat = res[0];
+        console.log(this.bat_stat);
+      }
+    );
+
+    this.server.get('/player/bowl_stat', { 'player_id': this.player_id }).subscribe(
+      res => {
+        this.bowl_stat = res[0];
+        console.log(this.bowl_stat);
+      }
+    );
   }
 
 }
